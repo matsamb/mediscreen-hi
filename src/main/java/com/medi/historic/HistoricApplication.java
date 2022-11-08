3025b846d550d178f3405d432f67eda43b836f22
matsamb <matondo.lusamba@gmail.com>
2022-11-08 12:21:52 +0100

    initial commit

---
+++ src/main/java/com/medi/historic/HistoricApplication.java
@@ -0,0 +1,13 @@
+package com.medi.historic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoricApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medi/historic/HistoricApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medi.historic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
