e51ce4209a941c790ac436c4aab25229fbaf2b10
matsamb <matondo.lusamba@gmail.com>
2022-12-11 17:12:03 +0100

    cors implemented

--- src/main/java/com/medi/historic/HistoricApplication.java
+++ src/main/java/com/medi/historic/HistoricApplication.java
@@ -1,16 +1,49 @@
 package com.medi.historic;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.medi.historic.config.MedConfigs;
+
+import lombok.extern.log4j.Log4j2;
 
 @SpringBootApplication
 //@EnableDiscoveryClient
 @EnableMongoRepositories
-public class HistoricApplication {
+@Log4j2
+public class HistoricApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HistoricApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+
+		log.info(medConfigs.getAngularurl());
+		
+	}
+	
+	@Autowired
+	private MedConfigs medConfigs;
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				
+				registry
+					.addMapping("/patient")
+					.allowedOrigins(medConfigs.getAngularurl()/*"localhost:4200""${medconfigs.angularurl}"*/);
+			}
+		};
 	}
 
 }
---
+++ src/main/java/com/medi/historic/config/MedConfigs.java
@@ -0,0 +1,24 @@
+package com.medi.historic.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("medconfigs")
+@Configuration
+//@RefreshScope
+public class MedConfigs {
+
+	private String angularurl;
+	
+	public String getAngularurl() {
+		return angularurl;
+	}
+	
+	public void setAngularurl(String url) {
+		this.angularurl = url;
+	}
+	
+}
